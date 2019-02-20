8dd6f634d437282b2ac5bb3df39b69f23381271b
Shehab Mohsen <shehab_mohsen451@yahoo.com>
2019-02-20 06:42:49 +0200

    intial

---
+++ plant/plant/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace plant.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
